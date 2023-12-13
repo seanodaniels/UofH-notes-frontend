125cc0f549453e16f1ba69c4009df9e022fc1116
Sean O'Daniels <sodaniels@gmail.com>
2023-12-13 09:26:29 -0500

    Update from part 5.1

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,16 @@ const App = () => {
     event.preventDefault()
 
     try {
-      const user = await loginService.login({ username, password })
+      const user = await loginService.login({ 
+        username, 
+        password 
+      })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+      
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -37,6 +46,15 @@ const App = () => {
       .then(initialNotes => {
         setNotes(initialNotes)
       })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
   }, [])
 
   const addNote = (event) => {
